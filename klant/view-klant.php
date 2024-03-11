f2a31af62c886a285d0dac721424f540ed5a2587
Deviously3011 <117723597+Deviously3011@users.noreply.github.com>
2024-03-11 13:15:52 +0100

    Update view-klant.php

--- klant/view-klant.php
+++ klant/view-klant.php
@@ -13,22 +13,38 @@ $alleklanten = $Klant->selectKlant()->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+ 
+    
 </head>
 <body>
-    <h1>Overzicht Klanten</h1>
-
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Naam</th>
-            <th> colspan "2"</th>
-        </tr>
-        <?php foreach ($alleklanten as $Klant) { ?>
-        <tr>
-            <td><?php echo $Klant['KlantID']; ?></td>
-            <td><?php echo $Klant['KlantNaam']; ?></td>
-        </tr>
-    <?php } ?> 
-    </table>
+    <div class="container">
+        <h1>Overzicht Klanten</h1>
+        
+            <table class="w-100 p-3 table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>ID</th>
+                        <th>Naam</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+              
+                    <?php foreach ($alleklanten as $Klant) { ?>
+                        <tr>
+                            <td><?php echo $Klant['KlantID']; ?></td>
+                            <td><?php echo $Klant['KlantNaam']; ?></td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button> 
+                                </td>
+                                <td>
+                                <button class="btn btn-danger">Delete</button>
+                            </td>
+                        </tr>
+                    <?php } ?>
+             
+            </table>
+        
+    </div>
 </body>
 </html>
